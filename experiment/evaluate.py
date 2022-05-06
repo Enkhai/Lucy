790d36de605db54380d1b73a5a7b9a2cdf10904a
Enkhai <aerogtx@gmail.com>
2022-05-07 01:57:59 +0300

    Fixed evaluate and added pickle5 to requirements to ensure model unpickling when loading

--- experiment/evaluate.py
+++ experiment/evaluate.py
@@ -1,14 +1,24 @@
 from rlgym.utils.state_setters import DefaultState
 
-from experiment.lucy_match_params import LucyReward, LucyTerminalConditions, LucyObs, LucyAction
+from lucy_match_params import LucyReward, LucyTerminalConditions, LucyObs, LucyAction
 from lucy_utils.load_evaluate import load_and_evaluate
 
 if __name__ == '__main__':
-    load_and_evaluate("../models_folder/Perceiver/model_449280000_steps.zip",
+    custom_objects = {
+        # pickle in Python 3.7 cannot parse stored schedule functions, we specify custom objects
+        'lr_schedule': 1e-4,
+        'clip_range': .2,
+        'n_envs': 1,  # need to specify one environment
+    }
+
+    kwargs = {'custom_objects': custom_objects}
+
+    load_and_evaluate("../models_folder/NectoReward_ownPerceiver_2x128_final/NectoReward_ownPerceiver_2x128_final.zip",
                       2,
                       LucyTerminalConditions(15),
-                      LucyObs(),
+                      LucyObs(stack_size=5),
                       DefaultState(),  # we use the default state for evaluation
                       LucyAction(),
-                      LucyReward(0.995)
+                      LucyReward(0.995),
+                      kwargs=kwargs
                       )
